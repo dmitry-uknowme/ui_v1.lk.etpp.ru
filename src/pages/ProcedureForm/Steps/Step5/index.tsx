08a4d043709abd831270f3833778df9a3434c1a1
Dmitry Bogatyrev <bogatyrov200488@gmail.com>
2022-12-21 12:05:34 +0500

    f

--- src/pages/ProcedureForm/Steps/Step5/index.tsx
+++ src/pages/ProcedureForm/Steps/Step5/index.tsx
@@ -386,13 +386,20 @@ const Step5 = ({ currentStep, setCurrentStep, nextStep, prevStep }) => {
     }
     const finalData = {
       lots: [
+        // ...(formGlobalValues?.lots?.length ? formGlobalValues.lots[0] : {}),
         {
-          plan_positions: formGlobalValues?.lots?.length && formGlobalValues?.lots[0]?.plan_positions?.length ? formGlobalValues?.lots[0]?.plan_positions : []
+          ...(formGlobalValues?.lots?.length
+            ? formGlobalValues.lots[0]
+            : {}),
+          plan_positions: formGlobalValues?.lots[0]?.plan_positions?.length ? formGlobalValues?.lots[0]?.plan_positions : [],
         },
+        // {
+        //   plan_positions: formGlobalValues?.lots?.length && formGlobalValues?.lots[0]?.plan_positions?.length ? formGlobalValues?.lots[0]?.plan_positions : []
+        // },
         // ...(formGlobalValues?.lots?.length && formGlobalValues?.lots[0]?.plan_positions?.length ? { plan_positions: formGlobalValues?.lots[0]?.plan_positions } : { plan_positions: [] })
-        {
-          ...(formGlobalValues?.lots?.length ? { ...formGlobalValues?.lots[0], plan_positions: formGlobalValues?.lots[0]?.plan_positions?.length ? formGlobalValues?.lots[0]?.plan_positions : [] } : {}),
-        },
+        // {
+        //   ...(formGlobalValues?.lots?.length ? { ...formGlobalValues?.lots[0], plan_positions: formGlobalValues?.lots[0]?.plan_positions?.length ? formGlobalValues?.lots[0]?.plan_positions : [] } : {}),
+        // },
       ],
       customer: {
         ogrn: formValue.customer_org_ogrn,
